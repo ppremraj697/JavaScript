244e0f9d87757d378ad307648e017f5500fc40ed
Premraj Patil <ppremraj697@gmail.com>
2024-01-17 23:41:24 +0530

    Strict mode in JS

---
+++ strict.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// 1. variable declaration
+// a = 12; --------------- ERROR : a is not declared/defined
+
+// 2. variable can't be deleted
+// let a = 12;
+// delete a; ------------- ERROR : delete cannot be called on an identifier in strict mode.
+
+// 3. can't make same parameters in function
+// function calc(a,b,b){ ----------- ERROR : Duplicate parameter name not allowed in this context
+
+// }
