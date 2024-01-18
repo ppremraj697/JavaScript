801dc8b304f46f752930cd0a603f32cb908c701d
Premraj Patil <ppremraj697@gmail.com>
2024-01-18 23:22:29 +0530

    basics of Array in JS

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var arr = ["Premraj", 20166, "DYPIEMR", 1, 2, "Pune"];
+
+arr.pop();//Delete last element
+arr.push(5);//Insert in last
+arr.unshift();//Remove first element
+arr.shift("Good Boy");//Insert in begineeing
+
+console.log(arr);
