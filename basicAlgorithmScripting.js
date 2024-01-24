da8facdacfb2e2b4077ad0640eaec8d30ade5dab
Premraj Patil <ppremraj697@gmail.com>
2024-01-24 22:13:05 +0530

    Basic Algorithm Scripting : Reverse String

---
+++ basicAlgorithmScripting.js
@@ -0,0 +1,10 @@
+//Reverse String
+function reverseString(str) {
+    var newStr = ""
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr;
+}
+
+console.log(reverseString("Greetings from Premraj"));
