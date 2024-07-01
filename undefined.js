d325ba79444d6b10e4a9512761847113ad86040c
Premraj Patil <ppremraj697@gmail.com>
2024-07-01 23:34:12 +0530

    Difference between undefined vs null vs not defined

---
+++ undefined.js
@@ -0,0 +1,13 @@
+//undefined: Variable has been declared but has not yet been assigned a value. It is also the default value of a function that does not reture anything.
+//Placeholder to reserve memory for a variable
+//type: undefined
+
+//null: It is a special value that means "NO VALUE". It can be explicitely assigned to a variable to indicate that the variable has no value.
+//type: object
+
+//not defined: Variable has not been allocated any memory / Variable not present in the program.
+//type: none
+
+console.log(undefined == null) //--> true 
+
+console.log(undefined === null) //-->false
